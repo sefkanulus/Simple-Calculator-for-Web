11679485e9112012719437c9a51290ac7bb3c93a
Sefkan Ulus <62238766+sefkanulus@users.noreply.github.com>
2024-03-29 18:22:35 +0300

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="process" runat="server" Height="100px"  Width="310px" Font-Size="32pt" OnTextChanged="process_TextChanged"></asp:TextBox>
+            <asp:Label runat="server" ID="Output" Text=""></asp:Label>
+        </div>
+        <asp:Button ID="Button1" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Button1_Click" Text="1" Width="70px" />
+        <asp:Button ID="Button2" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Button2_Click" Text="2" Width="70px" />
+        <asp:Button ID="Button3" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Button3_Click" Text="3" Width="70px" />
+        <asp:Button ID="Button4" runat="server" BackColor="#FF9999" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Buttonsum_Click" Text="+" Width="100px" />
+        <br />
+        <asp:Button ID="Button5" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Button4_Click" Text="4" Width="70px" />
+        <asp:Button ID="Button6" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Button5_Click" Text="5" Width="70px" />
+        <asp:Button ID="Button7" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Button6_Click" Text="6" Width="70px" />
+        <asp:Button ID="Button8" runat="server" BackColor="#FF9999" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Buttonsubtraction_Click" Text="-" Width="100px" />
+        <br />
+        <asp:Button ID="Button9" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Button7_Click" Text="7" Width="70px" />
+        <asp:Button ID="Button10" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Buttoneight_Click" Text="8" Width="70px" />
+        <asp:Button ID="Button11" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Buttonnine_Click" Text="9" Width="70px" />
+        <asp:Button ID="Button12" runat="server" BackColor="#FF9999" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Button12_Click" Text="*" Width="100px" />
+        <br />
+        <asp:Button ID="Button17" runat="server" BackColor="#CC0000" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Buttonformat_Click" Text="C" Width="70px" />
+        <asp:Button ID="Button14" runat="server" BackColor="#66FFFF" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Buttonzero_Click" Text="0" Width="70px" />
+        <asp:Button ID="Button15" runat="server" BackColor="#99FF33" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Buttonequal_Click" Text="=" Width="70px" />
+        <asp:Button ID="Button16" runat="server" BackColor="#FF9999" Font-Size="XX-Large" ForeColor="Black" Height="70px" OnClick="Buttondıvısıon_Click" Text="/" Width="100px" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static double number1, number2;
+        static double result;
+        static String mark;
+
+
+        protected void Ekleme(String number)
+        {
+            process.Text += number;
+        }
+
+        protected void Buttonequal_Click(object sender, EventArgs e)
+        {
+            number2 = Convert.ToDouble(process.Text);
+
+
+            if (mark == "+")
+            {
+                result = number1 + number2;
+                process.Text = Convert.ToString(result);
+            }
+
+            if (mark == "-")
+            {
+                result = number1 - number2;
+                process.Text = Convert.ToString(result);
+
+            }
+            if (mark == "*")
+            {
+                result = number1 * number2;
+                process.Text = Convert.ToString(result);
+
+            }
+            if (mark == "/")
+            {
+                result = number1 / number2;
+                process.Text = Convert.ToString(result);
+
+            }
+
+            Output.Text += Convert.ToString(number2);
+            Output.Text += "=" + result;
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Ekleme("1");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Ekleme("2");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Ekleme("3");
+        }
+
+        protected void Buttonsum_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(process.Text);
+            mark = "+";
+            process.Text = "";
+            Output.Text = Convert.ToString(number1) + mark;
+        }
+
+
+
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Ekleme("4");
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            Ekleme("5");
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            Ekleme("6");
+        }
+
+        protected void Buttonsubtraction_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(process.Text);
+            mark = "-";
+            process.Text = "";
+            Output.Text = Convert.ToString(number1) + mark;
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            Ekleme("7");
+        }
+
+        protected void Buttoneight_Click(object sender, EventArgs e)
+        {
+            Ekleme("8");
+        }
+
+        protected void Buttonnine_Click(object sender, EventArgs e)
+        {
+            Ekleme("9");
+        }
+
+        protected void Buttonmultiplication_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Buttonformat_Click(object sender, EventArgs e)
+        {
+            process.Text = "";
+            Output.Text = "";
+        }
+
+        protected void Buttonzero_Click(object sender, EventArgs e)
+        {
+            Ekleme("0");
+        }
+
+        protected void process_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(process.Text);
+            mark = "*";
+            process.Text = "";
+            Output.Text = Convert.ToString(number1) + mark;
+        }
+
+        protected void Buttondıvısıon_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(process.Text);
+            mark = "/";
+            process.Text = "";
+            Output.Text = Convert.ToString(number1) + mark;
+        }
+
+
+    }
+
+}
